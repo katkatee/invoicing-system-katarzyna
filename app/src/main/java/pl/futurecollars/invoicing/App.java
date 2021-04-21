811193548a5957b0084e79e2d36d3bc16d9cf85a
katkatee <45096745+katkatee@users.noreply.github.com>
2021-04-21 13:41:22 +0200

    json-task (#10)
    * json-task
    
    * update
    
    * Update build.gradle
    
    * Update App.java
    
    * Update Company.java
    
    * Update Company.java
    
    * Update Company.java
    
    * Update Company.java
    
    * Update Company.java
    
    * Update Company.java
    
    * Update Company.java
    
    * Delete db directory

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -1,35 +1,43 @@
 package pl.futurecollars.invoicing;
 
 import java.math.BigDecimal;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
+import pl.futurecollars.invoicing.config.Config;
 import pl.futurecollars.invoicing.db.Database;
-import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
+import pl.futurecollars.invoicing.db.file.FileBasedDatabase;
+import pl.futurecollars.invoicing.db.file.IdService;
 import pl.futurecollars.invoicing.model.Company;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.model.InvoiceEntry;
 import pl.futurecollars.invoicing.model.Vat;
 import pl.futurecollars.invoicing.service.InvoiceService;
+import pl.futurecollars.invoicing.utils.FilesService;
+import pl.futurecollars.invoicing.utils.JsonService;
 
 public class App {
 
   public static void main(String[] args) {
 
-    Database db = new InMemoryDatabase();
+    FilesService filesService = new FilesService();
+    IdService idService = new IdService(Path.of(Config.ID_FILE_LOCATION), filesService);
+    JsonService jsonService = new JsonService();
+    Database db = new FileBasedDatabase(Path.of(Config.DATABASE_LOCATION), idService, filesService, jsonService);
 
     InvoiceService invoiceService = new InvoiceService(db);
 
     Company buyer = new Company("5213861303", "ul. Bukowińska 24d/7 02-703 Warszawa, Polska", "iCode Trust Sp. z o.o");
     Company seller = new Company("552-168-66-00", "32-005 Niepolomice, Nagietkowa 19", "Piotr Kolacz Development");
 
-    List<InvoiceEntry> products = List.of(new InvoiceEntry("Programming course", BigDecimal.valueOf(10000), BigDecimal.valueOf(2300), Vat.VAT_23));
+    List<InvoiceEntry> products =
+        List.of(new InvoiceEntry("Programming course", 1, BigDecimal.valueOf(10000), BigDecimal.valueOf(2300), Vat.VAT_23));
 
     Invoice invoice = new Invoice(LocalDate.now(), buyer, seller, products);
 
     int id = invoiceService.save(invoice);
 
     invoiceService.getById(id).ifPresent(System.out::println);
-    System.out.println();
 
     System.out.println(invoiceService.getAll());
 
--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,20 +1,16 @@
 package pl.futurecollars.invoicing.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Company {
 
-  private String name;
   private String taxIdentificationNumber;
   private String address;
-
-  public Company(String name, String taxIdentificationNumber, String address) {
-    this.name = name;
-    this.taxIdentificationNumber = taxIdentificationNumber;
-    this.address = address;
-
-  }
+  private String name;
+  
 }
