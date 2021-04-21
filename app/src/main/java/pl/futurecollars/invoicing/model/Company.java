339a4dfcfcd6a5f05773923d92d8ffac74ac462f
katkatee <45096745+katkatee@users.noreply.github.com>
2021-04-21 08:31:49 +0200

    Update Company.java

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,12 +1,12 @@
 package pl.futurecollars.invoicing.model;
 
-import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.Data;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Company {
 
   private String taxIdentificationNumber;
