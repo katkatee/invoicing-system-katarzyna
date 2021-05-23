3ef0b6afa8ab101153256198e8a13791118404b2
katkatee <45096745+katkatee@users.noreply.github.com>
2021-05-23 12:01:15 +0200

    dependency injection (#16)

--- app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
@@ -18,7 +17,11 @@ import pl.futurecollars.invoicing.service.InvoiceService;
 @RequestMapping("invoices")
 public class InvoiceController {
 
-  private InvoiceService invoiceService = new InvoiceService(new InMemoryDatabase());
+  private final InvoiceService invoiceService;
+
+  public InvoiceController(InvoiceService invoiceService) {
+    this.invoiceService = invoiceService;
+  }
 
   @GetMapping
   public List<Invoice> getAll() {
